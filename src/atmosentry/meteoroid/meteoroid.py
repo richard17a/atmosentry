81210f6514be017e4ee132c0d719d992d726406c
Richard Anslow <r.anslow@outlook.com>
2024-11-22 15:35:38 +0000

    meteoroid docstrings

--- src/atmosentry/meteoroid/meteoroid.py
+++ src/atmosentry/meteoroid/meteoroid.py
@@ -1,33 +1,83 @@
-"""
-Add docstring...
-"""
+# pylint: disable=C0103
+# pylint: disable=W0102
 
-import numpy as np
+"""
+Module: meteoroid
+
+This module defines the Meteoroid class, which contains the physical properties, 
+and dynamic state of a meteoroid entering a planetary atmosphere. 
+
+At some point should come back and check the correct types in setters/getters....
+"""
 
 
 class Meteoroid():
     """
-    This is a meteroid object
+    Describes the physical properties, and trajectory of a meteoroid during atmospheric
+    entry. The object also tracks whether the meteoroid has generated child fragments.
+
+    Attributes:
+    -----------
+        x (float): The x-position of the meteoroid [m]
+        y (float): The y-position of the meteoroid [m]
+        z (float): The z-position of the meteoroid [m]
+        vx (float): The x-component of the meteoroid's velocity [m/s] 
+        vy (float): The y-component of the meteoroid's velocity [m/s] 
+        vz (float): The z-component of the meteoroid's velocity [m/s] 
+        radius (float): Radius of the meteoroid [m]]
+        mass (float): Mass of the meteoroid [kg]]
+        sigma (float): Tensile strength of the meteoroid [Pa]
+        rho (float): Bulk density of the meteoroid [kg/m^3]
+        eta (float): Heat of ablation of the meteoroid [J/kg]
+        dM (float, optional): The mass loss of the meteoroid [kg/s]
+        dEkin (float, optional): The energy loss of the meteoroid [J/s]
+        children (bool, optional): Meteoroid's child fragments (default: False)
+        t_init (float, optional): Initial simulation time [s] (default: 0)
+        t (list, optional): Simulation time
+
     """
 
-    def __init__(self, 
+    def __init__(self,
                  x,
                  y,
-                 z, 
-                 vx, 
-                 vy, 
-                 vz, 
-                 radius, 
-                 mass, 
+                 z,
+                 vx,
+                 vy,
+                 vz,
+                 radius,
+                 mass,
                  sigma,
                  rho,
                  eta,
                  dM=None,
-                 dEkin=None, 
+                 dEkin=None,
                  children=False,
                  t_init=0,
                  t=[]):
-        
+        """
+        Initializes a Meteoroid object with the given physical properties and initial state.
+
+        Args:
+        -----
+            x (float): Initial x-coordinate of the meteoroid [m]
+            y (float): Initial y-coordinate of the meteoroid [m]
+            z (float): Initial z-coordinate (altitude) of the meteoroid [m]
+            vx (float): Initial velocity in the x-direction [m/s]
+            vy (float): Initial velocity in the y-direction [m/s]
+            vz (float): Initial velocity in the z-direction [m/s]
+            radius (float): Radius of the meteoroid [m]]
+            mass (float): Mass of the meteoroid in [kg]
+            sigma (float): Tensile strength of the meteoroid [Pa]
+            rho (float): Bulk density of the meteoroid [kg/m^3]]
+            eta (float): Heat of ablation of the meteoroid [J/kg]
+            dM (float, optional): The mass loss of the meteoroid [kg/s]
+            dEkin (float, optional): The energy loss of the meteoroid [J/s]
+            children (bool, optional): Meteoroid's child fragments (default: False)
+            t_init (float, optional): Initial simulation time [s] (default: 0)
+            t (list, optional): Simulation time
+
+        """
+
         self.x = x
         self.y = y
         self.z = z
@@ -45,4 +95,162 @@ class Meteoroid():
         self.t_init = t_init
         self.t = t
 
-        # should add setters and getters here (and check mass, radius, density are consistent.....)
+    @property
+    def x(self):
+        """Getter for the meteoroid's x-coordinate."""
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        """Setter for the meteoroid's x-coordinate."""
+        self._x = value
+
+    @property
+    def y(self):
+        """Getter for the meteoroid's y-coordinate."""
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        """Setter for the meteoroid's y-coordinate."""
+        self._y = value
+
+    @property
+    def z(self):
+        """Getter for the meteoroid's z-coordinate."""
+        return self._z
+
+    @z.setter
+    def z(self, value):
+        """Setter for the meteoroid's z-coordinate."""
+        self._z = value
+
+    @property
+    def vx(self):
+        """Getter for the meteoroid's velocity in the x-direction."""
+        return self._vx
+
+    @vx.setter
+    def vx(self, value):
+        """Setter for the meteoroid's velocity in the x-direction."""
+        self._vx = value
+
+    @property
+    def vy(self):
+        """Getter for the meteoroid's velocity in the y-direction."""
+        return self._vy
+
+    @vy.setter
+    def vy(self, value):
+        """Setter for the meteoroid's velocity in the y-direction."""
+        self._vy = value
+
+    @property
+    def vz(self):
+        """Getter for the meteoroid's velocity in the z-direction."""
+        return self._vz
+
+    @vz.setter
+    def vz(self, value):
+        """Setter for the meteoroid's velocity in the z-direction."""
+        self._vz = value
+
+    @property
+    def radius(self):
+        """Getter for the meteoroid's radius."""
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        """Setter for the meteoroid's radius."""
+        self._radius = value
+
+    @property
+    def mass(self):
+        """Getter for the meteoroid's mass."""
+        return self._mass
+
+    @mass.setter
+    def mass(self, value):
+        """Setter for the meteoroid's mass."""
+        self._mass = value
+
+    @property
+    def sigma(self):
+        """Getter for the meteoroid's tensile strength."""
+        return self._sigma
+
+    @sigma.setter
+    def sigma(self, value):
+        """Setter for the meteoroid's tensile strength."""
+        self._sigma = value
+
+    @property
+    def rho(self):
+        """Getter for the meteoroid's bulk density."""
+        return self._rho
+
+    @rho.setter
+    def rho(self, value):
+        """Setter for the meteoroid's bulk density."""
+        self._rho = value
+
+    @property
+    def eta(self):
+        """Getter for the meteoroid's heat of ablation."""
+        return self._eta
+
+    @eta.setter
+    def eta(self, value):
+        """Setter for the meteoroid's heat of ablation."""
+        self._eta = value
+
+    @property
+    def dM(self):
+        """Getter for the meteoroid's mass loss rate."""
+        return self._dM
+
+    @dM.setter
+    def dM(self, value):
+        """Setter for the meteoroid's mass loss rate."""
+        self._dM = value
+
+    @property
+    def dEkin(self):
+        """Getter for the meteoroid's energy loss rate."""
+        return self._dEkin
+
+    @dEkin.setter
+    def dEkin(self, value):
+        """Setter for the meteoroid's energy loss rate."""
+        self._dEkin = value
+
+    @property
+    def children(self):
+        """Getter for the meteoroid's children fragments status."""
+        return self._children
+
+    @children.setter
+    def children(self, value):
+        """Setter for the meteoroid's children fragments status."""
+        self._children = value
+
+    @property
+    def t_init(self):
+        """Getter for the meteoroid's initial time."""
+        return self._t_init
+
+    @t_init.setter
+    def t_init(self, value):
+        """Setter for the meteoroid's initial time."""
+        self._t_init = value
+
+    @property
+    def t(self):
+        """Getter for the meteoroid's simulation time."""
+        return self._t
+
+    @t.setter
+    def t(self, value):
+        """Setter for the meteoroid's simulation time."""
+        self._t = value
